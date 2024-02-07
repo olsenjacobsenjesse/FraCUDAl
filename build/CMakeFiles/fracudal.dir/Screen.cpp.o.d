f901658e29b5142d775294ca9cd348bcca218a9d
olsenjacobsenjesse <94766816+olsenjacobsenjesse@users.noreply.github.com>
2024-02-06 23:09:49 -0800

    Delete build directory

--- build/CMakeFiles/fracudal.dir/Screen.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fracudal.dir/Screen.cpp.o: \
- /home/macchiato/programming/personal/cuda/cuda/fraCUDAl/Screen.cpp \
- /usr/include/stdc-predef.h \
- /home/macchiato/programming/personal/cuda/cuda/fraCUDAl/Screen.h
